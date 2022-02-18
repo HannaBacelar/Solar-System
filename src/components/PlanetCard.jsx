be3cb2663088d3efafcdee8366879de4e5fb13a5
Hanna Bacelar <hannabacelar@hotmail.com>
2022-02-18 20:01:34 -0300

    requisito 5 concertando, requisito 6

---
+++ src/components/PlanetCard.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class PlanetCard extends React.Component {
+  render() {
+    const { planetName, planetImage } = this.props;
+    return (
+      <div data-testid="planet-card">
+        <p data-testid="planet-name">{ planetName }</p>
+        <img src={ planetImage } alt={ `Planeta ${planetName}` } />
+      </div>
+
+    );
+  }
+}
+PlanetCard.propTypes = {
+  planetName: PropTypes.string.isRequired,
+  planetImage: PropTypes.string.isRequired,
+};
+export default PlanetCard;
--- src/components/SolarSystem.jsx
+++ src/components/SolarSystem.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
 import Title from './Title';
+import PlanetCard from './PlanetCard';
+import planets from '../data/planets';
 
 class SolarSystem extends React.Component {
   render() {
     return (
       <div data-testid="solar-system">
         <Title headline="Planetas" />
+        {
+          planets.map((planetas) => (
+            <PlanetCard
+              key={ planets.name }
+              planetName={ planetas.name }
+              planetImage={ planetas.image }
+            />
+          ))
+        }
       </div>
 
     );
