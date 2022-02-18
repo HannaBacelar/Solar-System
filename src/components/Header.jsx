dfcb269f75daaca69185df728c6a4e6388775ad6
Hanna Bacelar <hannabacelar@hotmail.com>
2022-02-18 16:11:49 -0300

    requisito 2

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Header extends React.Component {
+  render() {
+    return (
+      <header>
+        <h1 className="titulo">Sistema Solar</h1>
+      </header>
+    );
+  }
+}
+
+export default Header;
