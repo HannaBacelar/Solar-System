573b80e90afdeaba648b604a47a62519f5886b03
Hanna Bacelar <hannabacelar@hotmail.com>
2022-02-18 17:47:38 -0300

    requisito 5

--- src/components/SolarSystem.jsx
+++ src/components/SolarSystem.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import Title from './Title';
 
 class SolarSystem extends React.Component {
   render() {
     return (
-      <div data-testid="solar-system" />
+      <div data-testid="solar-system">
+        <Title headline="Planetas" />
+      </div>
+
     );
   }
 }
