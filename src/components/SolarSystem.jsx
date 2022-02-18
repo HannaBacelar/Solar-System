7fad76f038f484fc5454e6d9a362a40d2df6d6a6
Hanna Bacelar <hannabacelar@hotmail.com>
2022-02-18 16:37:12 -0300

    requisito 2

---
+++ src/components/SolarSystem.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+class SolarSystem extends React.Component {
+  render() {
+    return (
+      <div data-testid="solar-system" />
+    );
+  }
+}
+export default SolarSystem;
