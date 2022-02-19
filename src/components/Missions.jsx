218c311a83620cb8169ea7c7c630101fbbdf223b
Hanna Bacelar <hannabacelar@hotmail.com>
2022-02-18 21:08:50 -0300

    requisito 8

--- src/components/Missions.jsx
+++ src/components/Missions.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import Title from './Title';
 
 class Missions extends React.Component {
   render() {
     return (
-      <div data-testid="missions"> </div>
+      <div data-testid="missions">
+        <Title headline="Missões" />
+      </div>
+
     );
   }
 }
